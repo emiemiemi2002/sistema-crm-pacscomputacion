ddcade8238ee19c642c0b24f27f9d071b5d6b509
emiemiemi2002 <emilianouuu39@gmail.com>
2025-11-02 17:37:47 -0600

    Integración de modelos de apps a Django admin
     Changes to be committed:
    	modified:   catalogo/__pycache__/admin.cpython-313.pyc
    	modified:   catalogo/admin.py
    	modified:   db.sqlite3
    	modified:   gestion_clientes/__pycache__/admin.cpython-313.pyc
    	modified:   gestion_clientes/admin.py
    	modified:   gestion_ordenes/__pycache__/admin.cpython-313.pyc
    	modified:   gestion_ordenes/admin.py

--- gestion_ordenes/admin.py
+++ gestion_ordenes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import OrdenServicio, Cotizacion, Transferencia, ItemTransferido, BitacoraOrden
 
 # Register your models here.
+admin.site.register(OrdenServicio)
+admin.site.register(Cotizacion)
+admin.site.register(Transferencia)
+admin.site.register(ItemTransferido)
+admin.site.register(BitacoraOrden)
